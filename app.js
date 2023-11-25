a641e1a48893a7d56194b09615a9ad6c5e226495
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-11-25 11:55:00 -0300

    Desenvolvimento e finalização da Página Inicial

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const indexRouter = require('./app/routes/index.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'ejs');
+
+app.use('', indexRouter);
+
+app.listen(3000, ()=>
+{
+    console.log('Servidor Iniciado!');
+})
---
+++ public/scripts/layouts/menu.js
@@ -0,0 +1,130 @@
+const burgerButton = document.querySelector('.burgerButton');
+const menu = document.getElementsByTagName('header')[0];
+const menuList = document.querySelector('.menuOptions');
+
+const home = document.querySelector('.home'),
+notification = document.querySelector('.notification'),
+community = document.querySelector('.community'),
+aboutUs = document.querySelector('.aboutUs'),
+support = document.querySelector('.support'),
+settings = document.querySelector('.settings'),
+theme = document.querySelector('.theme'),
+userOption = document.querySelector('.userOption');
+
+home.addEventListener('mouseover', ()=>
+{
+    home.firstElementChild.src = "/assets/home.png";
+});
+home.addEventListener('mouseout', ()=>
+{
+    home.firstElementChild.src= "/assets/homeGray.png";
+})
+
+notification.addEventListener('mouseover', ()=>
+{
+    notification.firstElementChild.src = "/assets/notification.png";
+});
+notification.addEventListener('mouseout', ()=>
+{
+    notification.firstElementChild.src= "/assets/notificationGray.png";
+})
+
+community.addEventListener('mouseover', ()=>
+{
+    community.firstElementChild.src = "/assets/community.png";
+});
+community.addEventListener('mouseout', ()=>
+{
+    community.firstElementChild.src= "/assets/communityGray.png";
+})
+
+aboutUs.addEventListener('mouseover', ()=>
+{
+    aboutUs.firstElementChild.src = "/assets/aboutUs.png";
+});
+aboutUs.addEventListener('mouseout', ()=>
+{
+    aboutUs.firstElementChild.src= "/assets/aboutUsGray.png";
+})
+
+support.addEventListener('mouseover', ()=>
+{
+    support.firstElementChild.src = "/assets/support.png";
+});
+support.addEventListener('mouseout', ()=>
+{
+    support.firstElementChild.src= "/assets/supportGray.png";
+})
+
+settings.addEventListener('mouseover', ()=>
+{
+    settings.firstElementChild.src = "/assets/setting.png";
+});
+settings.addEventListener('mouseout', ()=>
+{
+    settings.firstElementChild.src= "/assets/settingGray.png";
+})
+
+theme.addEventListener('mouseover', ()=>
+{
+    theme.firstElementChild.src = "/assets/theme.png";
+});
+theme.addEventListener('mouseout', ()=>
+{
+    theme.firstElementChild.src= "/assets/themeGray.png";
+})
+
+userOption.addEventListener('mouseover', ()=>
+{
+    userOption.firstElementChild.src = "/assets/logout.png";
+});
+userOption.addEventListener('mouseout', ()=>
+{
+    userOption.firstElementChild.src= "/assets/logoutGray.png";
+})
+
+menuList.addEventListener('mouseover', ()=>
+{
+    menu.classList.remove('showResume');
+    menu.classList.add('showComplete');
+})
+
+menu.addEventListener('mouseout', ()=>
+{
+    if(menu.classList.contains('showComplete'))
+    {
+        menu.classList.remove('showComplete');
+        menu.classList.add('showResume');
+    }
+})
+
+burgerButton.addEventListener('click', () => {
+
+
+    if(event.target.classList.contains('open') && !menu.classList.contains('show'))
+    {
+        burgerButton.classList.remove('open');
+        burgerButton.classList.add('close');
+        menu.classList.add('showResume');
+    }
+    else
+    {
+        burgerButton.classList.remove('close');
+        burgerButton.classList.add('open');
+        menu.classList.remove('showResume');
+
+        if(menu.classList.contains('showComplete'))
+        {
+            menu.classList.remove('showComplete');
+        }
+    }
+
+    if(burgerButton.src == 'http://localhost:3000/assets/burger_open.svg')
+    {
+        burgerButton.src = 'http://localhost:3000/assets/burger_close.svg';
+    }
+    else
+    {
+        burgerButton.src = 'http://localhost:3000/assets/burger_open.svg';
+    }
+});
