9fb0a7afda291ea4484c097adaed7421acbf24f9
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-11-28 01:09:07 -0300

    Inicio do desenvolvimento da tela de pesquisa

--- public/scripts/search/index.js
+++ public/scripts/search/index.js
@@ -1,5 +1,30 @@
 const arrow = document.querySelector('.arrow');
 const searchBar = document.querySelector('.searchBar');
+const tags = document.querySelectorAll('.tag');
+const clearBtn = document.querySelector('.clear')
+const searchBtn = document.querySelector('.goButton');
+const input = document.querySelector('.searchInput');
+
+searchBar.addEventListener('click', ()=>
+{
+    input.value='';
+})
+
+tags.forEach(tag=>
+    {
+        tag.addEventListener('click', ()=>
+        {
+            tag.classList.toggle('checked');
+        })
+    })
+
+clearBtn.addEventListener('click', ()=>
+{
+    tags.forEach(tag=>
+        {
+            if(tag.classList.contains('checked')) tag.classList.remove('checked');
+        })
+})
 
 arrow.addEventListener('click', ()=>
 {
