0398fdcaf4bcf54b0d734f4413cc20d2ee40904d
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-12-01 15:16:57 -0300

    Finalização da tela de pesquisa

--- public/scripts/search/index.js
+++ public/scripts/search/index.js
@@ -1,6 +1,7 @@
 const arrow = document.querySelector('.arrow');
 const searchBar = document.querySelector('.searchBar');
 const tags = document.querySelectorAll('.tag');
+const tagContainer = document.querySelector('.tags');
 const clearBtn = document.querySelector('.clear')
 const searchBtn = document.querySelector('.goButton');
 const input = document.querySelector('.searchInput');
@@ -12,21 +13,61 @@ window.addEventListener('scroll', ()=>
 
     if(scrollTop < bannerHeight)
     {
+        tagContainer.classList.add('transparent');
         searchBar.classList.remove('principal');
-        arrow.classList.add('down');
-        arrow.classList.remove('up');
+        if(arrow.classList.contains('up'))
+        {
+            arrow.classList.remove('up');
+        }
+        searchBtn.classList.add('show');
+        clearBtn.classList.remove('show');
+        input.blur();
     }
     if (scrollTop > bannerHeight)
     {
         searchBar.classList.add('principal');
-        arrow.classList.remove('down');
-        arrow.classList.add('up');
+        if(!arrow.classList.contains('up'))
+        {
+            arrow.classList.add('up');
+        }
+        input.focus();
     } 
+    if(scrollTop > 0)
+    {
+        tagContainer.classList.remove('transparent');
+    }
 })
 
 searchBar.addEventListener('click', ()=>
 {
+    tagContainer.classList.remove('transparent');
+    searchBar.classList.add('principal');
+    var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    var scrollAmount = screenHeight * 0.49;
+    window.scrollTo(0, scrollAmount);
+})
+
+input.addEventListener('input', ()=>
+{
+    if (event.inputType === 'deleteContentBackward')
+    {
+        searchBtn.classList.remove('show');
+        clearBtn.classList.add('show');
+    }
+    else
+    {
+        searchBtn.classList.add('show');
+        clearBtn.classList.remove('show');
+    }
+})
+
+searchBtn.addEventListener('click', ()=>
+{
     input.value='';
+    tags.forEach(tag=>
+    {
+        if(tag.classList.contains('checked')) tag.classList.remove('checked');
+    })
 })
 
 tags.forEach(tag=>
@@ -40,28 +81,23 @@ tags.forEach(tag=>
 clearBtn.addEventListener('click', ()=>
 {
     tags.forEach(tag=>
-        {
-            if(tag.classList.contains('checked')) tag.classList.remove('checked');
-        })
+    {
+        if(tag.classList.contains('checked')) tag.classList.remove('checked');
+    })
+
+    input.value = '';
+    searchBtn.classList.add('show');
+    clearBtn.classList.remove('show');
 })
 
 arrow.addEventListener('click', ()=>
 {
-    if(arrow.classList.contains('down'))
-    {
-        var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        var scrollAmount = screenHeight * 0.49;
-        window.scrollTo(0, scrollAmount);
-
-        arrow.classList.remove('down');
-        arrow.classList.add('up');
-    }
-    else
-    {
-        window.scrollTo(scrollAmount, 0);
-        arrow.classList.remove('up');
-        arrow.classList.add('down');
-    }
-
+    tagContainer.classList.remove('transparent');
+    arrow.classList.remove('up');
     searchBar.classList.toggle('principal');
+
+    searchBtn.classList.add('show');
+    clearBtn.classList.remove('show');
+
+    window.scrollTo(500, 0);
 })
