4057020cc0f3dfe0baff29d215666aeccd539eb5
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-12-01 22:46:40 -0300

    Finalização da tela de pesquisa

--- public/scripts/paginaInicial/index.js
+++ public/scripts/paginaInicial/index.js
@@ -1,6 +1,12 @@
 const searchSection = document.querySelector('.box.searchBox');
+const categoriesSection = document.querySelector('.box.categoryBox');
 
 searchSection.addEventListener('click', ()=>
 {
     window.location.assign('/pesquisa');
+})
+
+categoriesSection.addEventListener('click', ()=>
+{
+    window.location.assign('/categorias');
 })
